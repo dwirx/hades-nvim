e40d9711ab32832a4e247ad9f39997d11cc17d5e
dwirx <bangundwir@gmail.com>
2024-02-27 22:33:07 +0700

    update

--- lua/hades/lazy/neotest.lua
+++ lua/hades/lazy/neotest.lua
@@ -1,34 +1,43 @@
-return {
-    {
-        "nvim-neotest/neotest",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "antoinemadec/FixCursorHold.nvim",
-            "nvim-treesitter/nvim-treesitter",
-            "marilari88/neotest-vitest",
-            "nvim-neotest/neotest-plenary",
-        },
-        config = function()
-            local neotest = require("neotest")
-            neotest.setup({
-                adapters = {
-                    require("neotest-vitest"),
-                    require("neotest-plenary").setup({
-                        -- this is my standard location for minimal vim rc
-                        -- in all my projects
-                        min_init = "./scripts/tests/minimal.vim",
-                    }),
-                }
-            })
-
-            vim.keymap.set("n", "<leader>tc", function()
-                neotest.run.run()
-            end)
-
-            vim.keymap.set("n", "<leader>tf", function()
-                neotest.run.run(vim.fn.expand("%"))
-            end)
-        end,
-    },
+local M = {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    -- general tests
+    "vim-test/vim-test",
+    "nvim-neotest/neotest-vim-test",
+    -- language specific tests
+    "marilari88/neotest-vitest",
+    "nvim-neotest/neotest-python",
+    "nvim-neotest/neotest-plenary",
+    "rouge8/neotest-rust",
+    "lawrence-laz/neotest-zig",
+    "rcasia/neotest-bash",
+  },
 }
 
+function M.config()
+  local wk = require "which-key"
+  wk.register {
+    ["<leader>tt"] = { "<cmd>lua require'neotest'.run.run()<cr>", "Test Nearest" },
+    ["<leader>tf"] = { "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", "Test File" },
+    ["<leader>td"] = { "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>", "Debug Test" },
+    ["<leader>ts"] = { "<cmd>lua require('neotest').run.stop()<cr>", "Test Stop" },
+    ["<leader>ta"] = { "<cmd>lua require('neotest').run.attach()<cr>", "Attach Test" },
+  }
+
+  ---@diagnostic disable: missing-fields
+  require("neotest").setup {
+    adapters = {
+      require "neotest-python" {
+        dap = { justMyCode = false },
+      },
+      require "neotest-vitest",
+      require "neotest-zig",
+      require "neotest-vim-test" {
+        ignore_file_types = { "python", "vim", "lua", "javascript", "typescript" },
+      },
+    },
+  }
+end
+
+return M
